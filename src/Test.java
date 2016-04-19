1c5f1897ee9ae26c98fae7a18f6f57913db822b6
mmlvkk <mmlvkk@gmail.com>
2016-04-19 17:50:12 +0800

    axis2 test

--- src/Test.java
+++ src/Test.java
@@ -17,8 +17,12 @@ public class Test {
 	public static void main(String[] args) throws MalformedURLException {
 		// TODO Auto-generated method stub
 		URL wsdlUrl = new URL("http://localhost:8080/hello?wsdl");
+//		URL wsdlUrl = new URL("http://127.0.0.1:8080/axis2/services/ServiceHello.ServiceHelloHttpEndpoint?wsdl");
+		
         Service s = Service.create(wsdlUrl, new QName("http://kwang.topsci.com/","ServiceHelloService"));
         ServiceHello hs = s.getPort(new QName("http://kwang.topsci.com/","ServiceHelloPort"), ServiceHello.class);
+        
+        
         String ret = hs.getValue("200 OK");
         System.out.println(ret);
 		
